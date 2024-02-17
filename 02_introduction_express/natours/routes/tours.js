4543017d9150042b234754d33633f5cc47cb80f5
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-18 04:00:25 +0530

    Ceate middleware for the specific request method, which only execute before the req forward to the req handler.

--- 02_introduction_express/natours/routes/tours.js
+++ 02_introduction_express/natours/routes/tours.js
@@ -7,8 +7,22 @@ const {saveTours,getTours ,deleteTours} = require('./../handlers/tour-handler');
 const router = express.Router();
 
 
+//Create custom middleware for the specific request method handler.
+const requestBodyCheckForPost = (req,res,next)=>{
+    console.log(req.body);
+    if(req.body.startLocation.description===null){
+        res.send({
+            status:'failed',
+            message:'Bad request body'
+        })
+    }
+    return;
+}
+
 // In this way we can chain routs in this fashion which are same url but different methods.
-router.route('/').post(saveTours);
+
+// Apply the middleware for the specific request handler.
+router.route('/').post(requestBodyCheckForPost,saveTours);
 
 router.route('/:id?').get(getTours).delete(deleteTours);
 
